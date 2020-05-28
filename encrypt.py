16ceed23fab6568fedcf618f7c5b4d9cdb56384d
ahmadtc1 <ahmadtc2000@gmail.com>
2020-05-27 21:30:51 -0400

    Adding logging to the encryption process

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,9 @@
 import cryptography
 from cryptography.fernet import Fernet
 import argparse
+import logging
+
+logging.basicConfig(filename="encrypt.log", level=logging.INFO, filemode='w')
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-k", "--api_key", required=True, help="your Bing Image Search API Key")
@@ -9,12 +12,20 @@ args = vars(ap.parse_args())
 
 #Generate a random key we use for our encryption (type <bytes>)
 key = Fernet.generate_key()
+logging.info("Successfully generated , writing key to file...")
 
-#Write the key to file so we can use it for decryption later
-file = open('key.key', 'wb')
-file.write(key)
+#Write the key to file so wes can use it for decryption later
+try:
+    file = open('key.key', 'wb')
+    file.write(key)
+except Exception as e:
+    print(e)
+    logging.info("Error occurred while writing cryptography key to file")
 file.close()
+logging.info("Key has been written to file")
 
+
+logging.info("Encrypting api key...")
 #Your API Key goes here to be encrypted
 message = args["api_key"].encode()
 
@@ -22,6 +33,12 @@ message = args["api_key"].encode()
 f = Fernet(key)
 encrypted = f.encrypt(message)
 
-file = open('api_key.encrypted', 'wb')
-file.write(encrypted)
+try:
+    file = open('api_key.encrypted', 'wb')
+    file.write(encrypted)
+except Exception as e:
+    print(e)
+    logging.info("Error occurred while writing encrypted api key to file")
 file.close()
+
+logging.info("Successfully encrypted and wrote api key to file")
