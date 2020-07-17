a9ca23c2a579d1f6c7a0689516ebbdf6bd9049e5
ahmadtc1 <ahmadtc2000@gmail.com>
2020-07-17 18:47:27 -0400

    Moving to GUI for encryption script.
    Deprecating command-line argument parsing for the encryption script.
    Instead moving towards a GUI to handle user input for a better user experience.
    Using PySimpleGUI (a wrapper for tkinter, qt, wxpython, and remi

--- encrypt.py
+++ encrypt.py
@@ -2,13 +2,23 @@ import cryptography
 from cryptography.fernet import Fernet
 import argparse
 import logging
+import PySimpleGUI as sg
 
 logging.basicConfig(filename="encrypt.log", level=logging.INFO, filemode='w')
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-k", "--api_key", required=True, help="your Bing Image Search API Key")
-args = vars(ap.parse_args())
+# Deprecating the command line argument parsing and using a GUI instead
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-k", "--api_key", required=True, help="your Bing Image Search API Key")
+# args = vars(ap.parse_args())
 
+sg.theme('Topanga')
+form = sg.FlexForm('API Key Encryption')
+layout = [[sg.Text('API Key', text_color='white', size=(25, 1)), 
+            sg.InputText("", text_color="white", key='api_key')],
+        [sg.Submit("Encrypt"), sg.Cancel()],
+	]
+window = sg.Window('datasetBuilder', layout)
+button,values = window.Read()
 
 #Generate a random key we use for our encryption (type <bytes>)
 key = Fernet.generate_key()
@@ -24,10 +34,9 @@ except Exception as e:
 file.close()
 logging.info("Key has been written to file")
 
-
 logging.info("Encrypting api key...")
 #Your API Key goes here to be encrypted
-message = args["api_key"].encode()
+message = values["api_key"].encode()
 
 #Use secret key authenticated cryptography
 f = Fernet(key)
@@ -42,3 +51,6 @@ except Exception as e:
 file.close()
 
 logging.info("Successfully encrypted and wrote api key to file")
+
+sg.Popup("Succesfully encrypted your API Key :)", title="Success")
+quit()
